5123d88813dc2caced9c5790bbb4cb56c90ebd8e
Jason <kangja1996@gmail.com>
2021-08-27 11:43:47 -0400

    finished

--- compareTriplets.js
+++ compareTriplets.js
@@ -1,20 +1,25 @@
 function compareTriplets(a, b) {
-  // Write your code here
   let resultArray = [0, 0];
 
   for (let i = 0; i < a.length; i++) {
-    for (let j = 0; j < b.length; j++) {
-      if (a[i] > b[j]) {
-        resultArray[0] = resultArray[0] + 1;
-      } else if (b[j] > a[i]) {
-        resultArray[1] = resultArray[1] + 1;
-      }
+    if (a[i] > b[i]) {
+      resultArray[0] = resultArray[0] + 1;
+    } else if (b[i] > a[i]) {
+      resultArray[1] = resultArray[1] + 1;
     }
   }
 
   return resultArray;
 }
 
-let a = [1, 2, 3];
-let b = [3, 2, 7];
+// let a = [1, 2, 3];
+// let b = [3, 2, 7];
+// console.log(compareTriplets(a, b));
+
+// let a = [5, 6, 7];
+// let b = [3, 6, 10];
+// console.log(compareTriplets(a, b));
+
+let a = [17, 28, 30];
+let b = [99, 16, 8];
 console.log(compareTriplets(a, b));
