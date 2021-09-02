25221b6dac23f4d9a9cd719b58a93657487e9fbe
Jason <kangja1996@gmail.com>
2021-09-02 16:50:26 -0400

    2nd solution for birthdayCakecandles finished

--- birthdayCakeCandles.js
+++ birthdayCakeCandles.js
@@ -4,21 +4,29 @@
 //  * The function accepts INTEGER_ARRAY candles as parameter.
 //  */
 
+// 1st solution
+// function birthdayCakeCandles(candles) {
+//   // Write your code here
+//   let sum = 0;
+//   let biggestNumber = Math.max(...candles);
+//   for (let i = 0; i < candles.length; i++) {
+//     if (candles[i] === biggestNumber) {
+//       sum = sum + 1;
+//     }
+//   }
+
+//   return sum;
+
+//   //   find the maximum number from the candles array
+//   // if the biggest number equals candles array, add +1 to the sum.
+//   // return the sum
+// }
+
+// 2nd solution
 function birthdayCakeCandles(candles) {
-  // Write your code here
-  let sum = 0;
   let biggestNumber = Math.max(...candles);
-  for (let i = 0; i < candles.length; i++) {
-    if (candles[i] === biggestNumber) {
-      sum = sum + 1;
-    }
-  }
-
-  return sum;
-
-  //   find the maximum number from the candles array
-  // if the biggest number equals candles array, add +1 to the sum.
-  // return the sum
+  return candles.filter((tallestCandle) => biggestNumber === tallestCandle)
+    .length;
 }
 
 candles = [3, 2, 1, 3];
