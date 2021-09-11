a21529ef853c36a0aef2c51207c8d849c94de164
Jason <kangja1996@gmail.com>
2021-09-11 16:35:43 -0400

    progresS

---
+++ kangaroo.js
@@ -0,0 +1,9 @@
+function kangaroo(x1, v1, x2, v2) {
+  // Write your code here
+}
+
+// x1 = starting location of the 1st kangaroo
+
+// x2 - starting location of the 2nd kangaroo
+
+// find the same location at the same time. If it's possible, return YES, otherwise return no.
