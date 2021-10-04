d66ec5f0321d8cee4ad403f7fb70c7a1502e5c3a
Jason <kangja1996@gmail.com>
2021-10-04 11:30:08 -0400

    solved

--- beautifulDays.js
+++ beautifulDays.js
@@ -10,4 +10,34 @@
 
 function beautifulDays(i, j, k) {
   // Write your code here
+  //   let i = 20;
+  //   let j = 23;
+  //   let k = 6;
+
+  let output = [];
+
+  for (let start = i; start <= j; start++) {
+    // convert each number in the range to a string. We do that by concatenating the number to an empty string.
+    // let numString = start + "";
+
+    // console.log(numString);
+    // 20;
+    // 21;
+    // 22;
+    // 23;
+
+    let reversed = start.toString().split("").reverse().join("");
+    // console.log(reversed);
+
+    if (Math.abs(start - reversed) % k === 0) {
+      output.push(start);
+    }
+  }
+
+  return output.length;
 }
+
+let i = 20;
+let j = 23;
+let k = 6;
+console.log(beautifulDays(i, j, k));
