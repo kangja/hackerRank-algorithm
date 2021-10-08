0b3097fcc249aeaf335ea18cb730dd167f39e5ad
Jason <kangja1996@gmail.com>
2021-10-08 00:15:43 -0400

    in progress

--- utopianTree.js
+++ utopianTree.js
@@ -7,40 +7,38 @@
 
 function utopianTree(n) {
   // Write your code here
-  //   initial height always starts at 1. and then multiply by 2 and then add 1 and this pattern repeats.
 
-  //   how to create an array from 0 - given number
-  let allNumber = [...Array(n + 1).keys()];
-  console.log(allNumber);
-  //   [0, 1, 2, 3, 4];
+  //   create a new height array
+  let height = [];
 
-  if (n === 0) {
-    console.log(1);
-  }
+  //   run for loop.
+  //   when            n = [0, 1, 2, 3];
+  //   height should equal [1, 2, 3, 6];
+  //   if n is an odd number, then height[i] equals to (height[n-1]) * 2
+  //   if n is an even number, then height[n] equals to (height[n-1]) + 1;
+  //     print height[i]
+  for (let i = 1; i < n; i++) {
+    let height = [];
 
-  // inside the for loop,
-  // if n is an odd number, multiply the previous number by 2 and return the number;
-  // if n is an even number, add 1 to the previous number
-
-  for (let i = 1; i <= allNumber.length; i++) {
-    if (n[i] % 2 === 1) {
-      let output = n[i - 1] * 2;
-      console.log(output);
-    }
-    if (n[i] % 2 === 0) {
-      let output = n[i - 1] + 1;
-      console.log(output);
+    //   if n === 0, print 1.
+    if (n === 0) {
+      height[0] = 1;
+      console.log(height[0]);
+    } else if (n % 2 !== 0) {
+      height[i] = height[i - 1] * 2;
+      console.log(height[i]);
+    } else if (n % 2 === 0) {
+      height[i] = height[i - 1] + 1;
+      console.log(height[i]);
     }
   }
 }
 
-// output should be
-// 1
-// 2
-// 7
+// if n = 0, then height = 1
+// if n = 1, then height = 2
+// if n = 2, then height = 3
+// if n = 3, then height = 6
+// if n = 4, then height = 7
+// if n = 5, then height = 14
 
-// let n = 0;
-// let n = 1;
-// let n = 4;
-
-utopianTree(n);
+utopianTree(1);
