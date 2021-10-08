25f5f0f17b0304cd461d3c1d92c83b06929750d1
Jason <kangja1996@gmail.com>
2021-10-08 00:59:08 -0400

    add new problem

---
+++ findDigits.js
@@ -0,0 +1,17 @@
+/*
+ * Complete the 'findDigits' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER n as parameter.
+ */
+
+function findDigits(n) {
+  // Write your code here
+  //   initialize count = 0
+  //   n = 1012;
+  //   how to separatre numbers into one digit?
+  //   divide n in to all separate numbers like 1, 0, 1, 2
+  //   if n % each number === 0 {
+  //      return all the count
+  //   }
+}
