7a1a19f60b080c7994fb7b1b891bddc78e7260d5
Jason <kangja1996@gmail.com>
2021-10-15 17:21:12 -0400

    new problem

---
+++ saveThePrisoner.js
@@ -0,0 +1,63 @@
+function saveThePrisoner(n, m, s) {}
+
+
+// who will receive the awful taste candy?
+
+// n = # of prisoners;
+// m = # of candy;
+// s = where the distribution starts at
+
+// ************* example 
+let n = 4;
+let m = 6;
+let s = 2;
+
+prisoners =             1, 2, 3, 4,
+distribution starts =      1, 2, 3
+                        4, 5, 6.
+
+return 6
+// ************* example
+let n = 5;
+let m = 2;
+let s = 1;
+
+prisoners =             1, 2, 3, 4, 5
+distribution starts =   1, 2
+
+return 2
+// ************* example
+let n = 5;
+let m = 2;
+let s = 2;
+
+prisoners =             1, 2, 3, 4, 5
+distribution starts =      1, 2
+
+return 3
+// ************* example
+let n = 7;
+let m = 19;
+let s = 2;
+
+prisoners =             1, 2, 3, 4, 5, 6, 7
+distribution starts =      1, 2, 3, 4, 5, 6,
+                        7, 8, 9, 10,11,12,13
+                       14,15,16, 17,18,19
+
+return 6
+// ************* example
+let n = 3;
+let m = 7;
+let s = 3;
+
+prisoners =             1, 2, 3
+distribution starts =         1
+                        2, 3, 4
+                        5, 6, 7
+
+return 3
+
+
+
+
