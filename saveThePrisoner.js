1705d3fec57d393534f5828725c4f8ed17f3fe5a
Jason <kangja1996@gmail.com>
2021-10-16 01:22:22 -0400

    finished

--- saveThePrisoner.js
+++ saveThePrisoner.js
@@ -1,4 +1,7 @@
-function saveThePrisoner(n, m, s) {}
+function saveThePrisoner(n, m, s) {
+    // (m-1 + s)%n = (2-1+2) % 5 = 3
+return (m-1 + s) % n || n
+}
 
 
 // who will receive the awful taste candy?
@@ -12,15 +15,25 @@ let n = 4;
 let m = 6;
 let s = 2;
 
+(m-1 + s)%n = (6-1+2) % 4 = 3
+
 prisoners =             1, 2, 3, 4,
 distribution starts =      1, 2, 3
                         4, 5, 6.
 
-return 6
+index   =               [0,1,2];
+
+
+m % n  = 
+6 % 4 = 2
+
+return 3
 // ************* example
 let n = 5;
 let m = 2;
 let s = 1;
+
+(m-1 + s)%n = (2-1+1) % 5 = 2
 
 prisoners =             1, 2, 3, 4, 5
 distribution starts =   1, 2
@@ -30,6 +43,9 @@ return 2
 let n = 5;
 let m = 2;
 let s = 2;
+
+(m-1 + s)%n = (2-1+2) % 5 = 3
+
 
 prisoners =             1, 2, 3, 4, 5
 distribution starts =      1, 2
@@ -45,6 +61,10 @@ distribution starts =      1, 2, 3, 4, 5, 6,
                         7, 8, 9, 10,11,12,13
                        14,15,16, 17,18,19
 
+
+(m-1 + s)%n = (19-1+2) % 7 = 6
+
+
 return 6
 // ************* example
 let n = 3;
@@ -56,8 +76,7 @@ distribution starts =         1
                         2, 3, 4
                         5, 6, 7
 
+(m-1 + s)%n = (7-1+3) % 3 = 6 || 3
+
+
 return 3
-
-
-
-
